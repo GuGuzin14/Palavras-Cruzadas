37cbf35c25349af021d58703e83bdcdd0f3009bf
Gustavo Henrique <gustavobispocosta5521@gmail.com>
2024-08-07 16:49:48 -0300

    Transformação em Vue/Escala da tabela

---
+++ CrossWords/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <CrossWords />
+  </div>
+</template>
+
+<script>
+import CrossWords from './components/CrossWords.vue';
+
+export default {
+  name: 'App',
+  components: {
+    CrossWords,
+  },
+};
+</script>
+
+<style>
+/* Adicione seus estilos globais aqui */
+</style>
--- README.md
+++
@@ -1,1 +0,0 @@
-# Palavras-Cruzadas
