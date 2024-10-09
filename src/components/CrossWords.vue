4f555c40591fa4fea0c665c554a691a6215ce7bf
gustavo <gustavobispocosta5521@gmail.com>
2024-10-09 17:30:52 -0300

    Refatorando Código

--- src/components/CrossWords.vue
+++ src/components/CrossWords.vue
@@ -1,14 +1,11 @@
 <template>
   <div class="container is-responsive">
     <div class="box" id="main-elements">
-      
-
       <div class="header">
         <button class="button is-link is-medium is-fullwidth is-responsive" @click="generateCrossWords">Criar</button>
         <br>
         <button class="button is-link is-medium is-fullwidth is-responsive" @click="checkWords">Checar Palavras</button>
       </div>
-
 
       <div id="inputTips">
         <h1>Dicas:</h1>
@@ -19,7 +16,15 @@
       </div>
     </div>
 
-    <div id="table-container" v-html="tableHTML"></div>
+    <div id="table-container">
+      <div class="table">
+        <div class="col" v-for="col in cols" :key="col">
+          <div class="row" v-for="row in rows" :key="row">
+            <input type="text" class="inputLetter" :id="`input_${col}_${row}`" maxLength="1" :tabindex="(col - 1) * rows + row">
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -31,48 +36,18 @@ export default {
       cols: 10,
       selectedRows: 10,
       selectedCols: 10,
-      tableHTML: '',
       tipMap: {},
       nextNumber: 1,
-      isCreated: false,
+      isCreated: true,
       initialArray: [],
-      isGenerated: false, // Nova flag para controlar se o cruzadama foi gerado
+      isGenerated: false,
     };
   },
   methods: {
-    createTable(rows, cols) {
-      let cont = 1;
-      console.log('Criando a tabela com', rows, 'linhas e', cols, 'colunas');
-      let tableHTML = '<div class="table">';
-      for (let i = 0; i < cols; i++) {
-        tableHTML += '<div class="col">';
-        for (let j = 0; j < rows; j++) {
-          const inputID = `input_${i}_${j}`;
-          tableHTML += `<div class="row">
-                          <input type="text" class="inputLetter" id="${inputID}" maxLength="1" tabindex="${cont}">
-                        </div>`;
-          cont++;
-        }
-        tableHTML += '</div>';
-      }
-      tableHTML += '</div>';
-      this.tableHTML = tableHTML;
-      this.isCreated = true;
+    addNumberToCell(event) {
+      if (this.isGenerated) return;
 
-      this.$nextTick(() => {
-        if (!this.isGenerated) {
-          this.addArrowMovement();
-          document.querySelectorAll('.inputLetter').forEach(input => {
-            input.addEventListener('dblclick', (event) => {
-              this.addNumberToCell(event.target);
-            });
-          });
-        }
-      });
-    },
-    addNumberToCell(input) {
-      if (this.isGenerated) return; // Não permite adicionar números se o cruzadama foi gerado
-
+      const input = event.target;
       const numberSpan = input.closest('.row').querySelector('.number');
       if (numberSpan) {
         const number = parseInt(numberSpan.textContent, 10);
@@ -105,10 +80,6 @@ export default {
       const inputTip = this.tipMap[number];
       if (inputTip) {
         inputTip.remove();
-        const label = inputTip.previousSibling;
-        if (label) {
-          label.remove();
-        }
         delete this.tipMap[number];
       }
     },
@@ -132,11 +103,8 @@ export default {
       });
     },
     updateTipLabel(oldNumber, newNumber) {
-      const label = document.querySelector(`label[for="inputTip_${oldNumber}"]`);
       const input = document.getElementById(`inputTip_${oldNumber}`);
-      if (label && input) {
-        label.innerHTML = `Dica ${newNumber}: `;
-        label.setAttribute('for', `inputTip_${newNumber}`);
+      if (input) {
         input.setAttribute('id', `inputTip_${newNumber}`);
         this.tipMap[newNumber] = input;
         delete this.tipMap[oldNumber];
@@ -144,12 +112,9 @@ export default {
     },
     generateCrossWords() {
       if (!this.isCreated) {
-        alert('Por favor, crie o crucigrama antes de gerar.');
         return;
       }
-
-      this.isGenerated = true; // Define que o cruzadama foi gerado
-
+      this.isGenerated = true;
       this.$nextTick(() => {
         const inputs = document.querySelectorAll('#table-container input');
         const table = document.querySelector('#table-container .table');
@@ -217,20 +182,19 @@ export default {
         rows.forEach((row, y) => {
           const input = row.querySelector('.inputLetter');
           if (input) {
-            currentArray[x][y] = input.value.trim().toUpperCase(); // Normaliza para maiúsculas
+            currentArray[x][y] = input.value.trim().toUpperCase();
           }
         });
       });
 
       console.log('Array atual das palavras inseridas:', currentArray);
 
-
       for (let i = 0; i < this.initialArray.length; i++) {
         for (let j = 0; j < this.initialArray[i].length; j++) {
           const inputValue = currentArray[i][j];
           const initialValue = this.initialArray[i][j];
 
-          const input = document.querySelector(`#input_${i}_${j}`);
+          const input = document.querySelector(`#input_${i + 1}_${j + 1}`);
           if (input) {
             if (inputValue === initialValue) {
               input.style.backgroundColor = 'lightgreen';
@@ -242,79 +206,86 @@ export default {
       }
     },
     addArrowMovement() {
-    document.querySelectorAll('.inputLetter').forEach(input => {
-      input.addEventListener('keydown', (event) => {
-        if (event.key.startsWith('Arrow')) {
-          this.arrowMovement(event);
-        }
+      document.querySelectorAll('.inputLetter').forEach(input => {
+        input.addEventListener('keydown', (event) => {
+          if (event.key.startsWith('Arrow')) {
+            this.arrowMovement(event);
+          }
+        });
+        input.addEventListener('dblclick', this.addNumberToCell);
       });
-    });
-  },
+    },
+    arrowMovement(event) {
+      event.preventDefault();
 
-  arrowMovement(event) {
-    event.preventDefault(); // Evitar o comportamento padrão
+      const currentInput = event.target;
+      const currentTabIndex = parseInt(currentInput.tabIndex, 10);
+      let newTabIndex;
 
-    const currentInput = event.target;
-    const currentTabIndex = parseInt(currentInput.tabIndex, 10);
-    let newTabIndex;
+      switch (event.key) {
+        case 'ArrowUp':
+          newTabIndex = currentTabIndex - 1;
+          break;
+        case 'ArrowDown':
+          newTabIndex = currentTabIndex + 1;
+          break;
+        case 'ArrowLeft':
+          newTabIndex = currentTabIndex - 10;
+          break;
+        case 'ArrowRight':
+          newTabIndex = currentTabIndex + 10;
+          break;
+        default:
+          return;
+      }
 
-    switch (event.key) {
-      case 'ArrowUp':
-        newTabIndex = currentTabIndex - 1;
-        break;
-      case 'ArrowDown':
-        newTabIndex = currentTabIndex + 1;
-        break;
-      case 'ArrowLeft':
-        newTabIndex = currentTabIndex - 10;
-        break;
-      case 'ArrowRight':
-        newTabIndex = currentTabIndex + 10;
-        break;
-      default:
-        return;
-    }
-
-    // Verifique se o novo tabindex é válido
-    const newInput = document.querySelector(`.inputLetter[tabindex="${newTabIndex}"]`);
-    if (newInput) {
-      newInput.focus();
-    }
-  },
+      const newInput = document.querySelector(`.inputLetter[tabindex="${newTabIndex}"]`);
+      if (newInput) {
+        newInput.focus();
+      }
+    },
     resetGame() {
       console.log('Resetando o jogo...');
       this.isGenerated = false;
       this.isCreated = false;
       this.tipMap = {};
       this.nextNumber = 1;
-      this.tableHTML = ''; // Limpa o HTML da tabela
 
+      // Remove all tip inputs and tip texts
       const inputsTip = document.querySelectorAll('.tip-input');
       const tipsText = document.querySelectorAll('#tip-text');
 
-      // console.log(inputsTip);
-      // console.log(tipsText);
-
       inputsTip.forEach((inputTip) => {
         inputTip.remove();
-      })
+      });
 
       tipsText.forEach((tipText) => {
         tipText.remove();
-      })
-      // inputsTip.remove();
-      // tipText.remove(); 
-
-      // Força uma nova criação da tabela
-      this.$nextTick(() => {
-        this.createTable(this.rows, this.cols);
       });
-      
+
+      // Clear all input fields and reset their styles
+      const inputs = document.querySelectorAll('.inputLetter');
+      inputs.forEach((input) => {
+        input.value = '';
+        input.style.backgroundColor = '';
+        input.readOnly = false;
+        input.closest('.row').style.visibility = 'visible';
+      });
+
+      // Remove all number spans
+      const numberSpans = document.querySelectorAll('.number');
+      numberSpans.forEach((span) => {
+        span.remove();
+      });
+
+      this.$nextTick(() => {
+        this.isCreated = true;
+      });
+
       console.log('Tabela recriada após reset.');
     },
   },
   mounted() {
-    this.createTable(this.rows, this.cols);
     this.addArrowMovement();
   }
 };
